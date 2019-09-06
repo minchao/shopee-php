5b3c9d828e27d57cf19f147c1bcfb16c6f577c1b
Choo Wei Ken <weikenchoo@gmail.com>
2019-09-06 17:37:13 +0800

    Updated getAttributeId method

--- src/Nodes/Item/Parameters/Attribute.php
+++ src/Nodes/Item/Parameters/Attribute.php
@@ -8,7 +8,7 @@ class Attribute extends RequestParameters
 {
     public function getAttributeId(): int
     {
-        return $this->parameters['attribute_id'];
+        return $this->parameters['attributes_id'];
     }
 
     /**
