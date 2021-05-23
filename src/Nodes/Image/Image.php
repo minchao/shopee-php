31e92fdbaf7e302dcc8bbdcc95ed9b5e8d6a6e7d
Minchao <minchao.220@gmail.com>
2021-05-23 20:16:25 +0800

    Update Image.php

--- src/Nodes/Image/Image.php
+++ src/Nodes/Image/Image.php
@@ -8,7 +8,8 @@ use Shopee\ResponseData;
 class Image extends NodeAbstract
 {
     /**
-     * Use this optional API to pre-validate your image urls and convert them to Shopee image url to use in item upload APIs.
+     * Use this optional API to pre-validate your image urls and convert them to Shopee image url to use in item
+     * upload APIs.
      * This way your potential invalid urls will not block your item upload process.
      * @param  array  $parameters
      * @return ResponseData
